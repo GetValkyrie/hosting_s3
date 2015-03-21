63912b58d0b876e8c7651372a3c78fcec9c0cf12
Christopher Gervais <chris@ergonlogic.com>
2015-03-21 06:20:47 +0000

    Move bucket name generation and validation to the backend.

--- drush/Provision/Service/s3.php
+++ drush/Provision/Service/s3.php
@@ -1,4 +1,5 @@
 <?php
+require_once dirname(__FILE__) . '/../../../includes/s3_common.inc';
 
 /**
  * The s3 service class.
@@ -42,4 +43,114 @@ class Provision_Service_s3 extends Provision_Service {
     // Do nothing here. The function is needed because it's always called,
     // but the actual implementation will be in the storage engine.
   }
+
+  /**
+   * Wrapper around drush_HOOK_pre_provision_verify().
+   */
+  function pre_verify() {
+  }
+
+  /**
+   * Wrapper around drush_HOOK_provision_install_validate().
+   */
+  function install_validate() {
+    if ($this->validate_credentials()) {
+      return $this->validate_bucket_name();
+    }
+    return FALSE;
+  }
+
+  /**
+   * Ensure provided credentials are complete and valid.
+   */
+  function validate_credentials() {
+    $access_key_id = d()->s3_access_key_id;
+    $secret_access_key = d()->s3_secret_access_key;
+    if (aegir_s3_credentials_exist($access_key_id, $secret_access_key, 'handle_missing_keys', $this)) {
+      $client = aegir_s3_client_factory($access_key_id, $secret_access_key);
+      return aegir_s3_validate_credentials($client, 'handle_validation', 'handle_exception', $this);
+    }
+    else {  // Missing key.
+      return FALSE;
+    }
+  }
+
+  /**
+   * Error handler for missing credentials.
+   */
+  function handle_missing_keys($missing_keys) {
+    foreach ($missing_keys as $key => $label) {
+      drush_set_error('ERROR_' . strtoupper($key) . '_MISSING', "Both S3 credentials are required. `$label` is blank.");
+    }
+    return FALSE;
+  }
+
+  /**
+   * Success handler for valid credentials.
+   */
+  function handle_validation() {
+    drush_log('S3 credentials validated.', 'ok');
+    return TRUE;
+  }
+
+  /**
+   * Error handler for API exceptions.
+   */
+  function handle_exception($exception) {
+    $code = $exception->getExceptionCode();
+    $message= $exception->getMessage();
+    $error = array();
+    $error[] = t('There was an error validating your S3 credentials.');
+    $error[] = t('Error code: @code', array('@code' => $code));
+    $error[] = t('Error message: @message', array('@message' => $message));
+    drush_set_error('ERROR_S3_EXCEPTION', implode('</li><li>', $error));
+    return FALSE;
+  }
+
+  /**
+   * Ensure generated bucket name is valid for S3.
+   */
+  function validate_bucket_name(){
+    $access_key_id = d()->s3_access_key_id;
+    $secret_access_key = d()->s3_secret_access_key;
+    $client = aegir_s3_client_factory($access_key_id, $secret_access_key);
+
+    if ($bucket_name = $this->suggest_bucket_name($client, d()->uri)) {
+      if ($client->isValidBucketName($bucket_name)) {
+        // Pass the bucket name to the front-end.
+        // See: hosting_s3_post_hosting_install_task().
+        drush_set_option('s3_bucket_name', $bucket_name);
+        // Save the bucket name to the context.
+        d()->s3_bucket_name = $bucket_name;
+        d()->write_alias();
+        drush_log(dt('S3 bucket name set to %bucket.', array('%bucket' => $bucket_name)), 'ok');
+        return TRUE;
+      }
+      else {
+        drush_set_error('ERROR_INVALID_S3_BUCKET_NAME', dt('Suggested bucket name (%bucket) failed S3 validation.', array('%bucket' => $bucket_name)));
+      }
+    }
+  }
+
+  /**
+   * Suggest an available, unique bucket name based on a site's URL.
+   */
+  function suggest_bucket_name($client, $url) {
+    $suggest_base = str_replace('.', '-', gethostname() . '-' . $url);
+
+    if (!$client->doesBucketExist($suggest_base)) {
+      return $suggest_base;
+    }
+
+    for ($i = 0; $i < 100; $i++) {
+      $option = $suggest_base . $i;
+      if (!$client->doesBucketExist($option)) {
+        return $option;
+      }
+    }
+
+    drush_set_error('ERROR_S3_BUCKET_NAME_SUGGESTIONS_FAILED', dt("Could not find a free bucket name after 100 attempts"));
+    return false;
+  }
+
 }
---
+++ drush/Provision/Service/s3/s3.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * The s3 service class.
+ */
+class Provision_Service_s3_s3 extends Provision_Service_s3 {
+}
