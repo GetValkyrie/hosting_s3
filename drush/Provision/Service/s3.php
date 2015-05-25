dc3e663808090f53a6b42f3236b35eae6096abf7
Christopher Gervais <chris@ergonlogic.com>
2015-05-25 04:29:14 +0000

    Remove legacy directory alters.

--- drush/Provision/Service/s3.php
+++ drush/Provision/Service/s3.php
@@ -29,19 +29,6 @@ class Provision_Service_s3 extends Provision_Service {
     // Create sites/$url and ensure permissions are set (remove from $dirs).
     // This helper can be called optionally to ensure that the site directory
     // is created appropriately before applying other modifications.
-/*
-    $path = 'sites/' . $url;
-    if (!is_dir($path)) {
-      provision_file()->mkdir($path)
-        ->succeed('Created <code>@path</code>')
-        ->fail('Could not create <code>@path</code>', 'DRUSH_PERM_ERROR');
-    }
-
-    provision_file()->chmod($path, $dirs[$path], false)
-      ->succeed('Changed permissions of <code>@path</code> to @perm')
-      ->fail('Could not change permissions <code>@path</code> to @perm');
-    unset($dirs[$path]);
-*/
   }
 
   /**
